6f7824681b9a1dbb2ccc775265b00f2dc087d973
TynysA <jjakellucas@gmail.com>
2023-11-08 19:33:32 +0600

    ios

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,31 +1,107 @@
 import { Component, OnInit } from '@angular/core';
-import { Contacts, ContactPayload } from '@capacitor-community/contacts';
+import { Contacts, EmailType, PhoneType } from '@capacitor-community/contacts';
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
-  contacts: ContactPayload[] = [];
+  contacts: any[] = [];
+  // result: any;
+  // contact: any;
+
   constructor() {}
-  ngOnInit(): void {
-    this.loadContacts();
+
+  ngOnInit() {
+    this.getContacts();
   }
 
-  async loadContacts() {
-    // if (isPlatform('android')) {
-    //   let permission = await Contacts.getPermissions();
-    //   if (!permission.granted) {
-    //     return;
-    //   }
-    // }
-
-    Contacts.getContacts().then((contacts) => {
-      this.contacts = contacts;
-    });
-    // Contacts.getContacts().then((result) => {
-    //   console.log(result);
-    //   this.contacts = result.contacts;
-    // });
+  async getContacts() {
+     try {
+      const permission = await Contacts.requestPermissions();
+      console.log('permission: ', permission.contacts);
+      if(!permission?.contacts) return;
+      else if(permission?.contacts == 'granted') {
+        const result = await Contacts.getContacts({
+          projection: {
+            name: true,
+            phones: true,
+            emails: true,
+            image: true
+          }
+        });
+        console.log('result: ', result);
+        this.contacts = result.contacts;
+        console.log(this.contacts);
+      }
+    } catch(e) {
+      console.log(e);
+    }
   }
+
+  // joinNumbers(array: any) {
+  //   return array.map((x: any) => x.number).join(' | ');
+  // }
+  // async createContact() {
+  //   try {
+  //     const result = await Contacts.createContact({
+  //       contact: {
+  //         name: {
+  //           given: 'Banking',
+  //           family: ' Technyks',
+  //         },
+  //         phones: [
+  //           {
+  //             type: PhoneType.Home,
+  //             number: '9101010110',
+  //           },
+  //           {
+  //             type: PhoneType.Work,
+  //             number: '9101010111',
+  //           },
+  //         ],
+  //         emails: [
+  //           {
+  //             type: EmailType.Home,
+  //             address: 'bankingtechnyks@gmail.com',
+  //           },
+  //         ],
+  //       },
+  //     });
+  //     // this.result = result;
+  //     const new_contact = await this.getContact(result?.contactId);
+  //     // this.contact = new_contact;
+  //     this.contacts.push(new_contact?.contact);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // async getContact(contactId: any) {
+  //   const new_contact = await Contacts.getContact({
+  //     contactId: contactId,
+  //     projection: {
+  //       name: true,
+  //       phones: true,
+  //       emails: true,
+  //       image: true,
+  //     },
+  //   });
+  //   // this.contact = new_contact;
+  //   return new_contact;
+  // }
+
+
+  // async pickContact() {
+  //   const picked_contact = await Contacts.pickContact({
+  //     projection: {
+  //       name: true,
+  //       phones: true,
+  //       emails: true,
+  //       image: true,
+  //     },
+  //   });
+  //   // this.contact = picked_contact;
+  //   return picked_contact;
+  // }
 }
